corrected wrong shape in discriminator
Must be very careful with the sizes in both the generator and the discrimintor.
The ground truth labels are actually based on the shape of logits themselves, so
an error in the logits would not be thrown. Previously, I assumed that the last
layer in the discriminator would have a shape of [-1, 4, 4, 256], but it was
wrong and should have been [-1, 7, 7, 256]. The wrong shape resulted in the
vector of logits being reshaped to [392, 1], instead of the batch size of [128,
1], which thus contributed to the GAN learning crap. Have not verified if it
resolves the issue below.

Unresolved:
1. Generator and Discriminator not training properly.

dcgan.py
@@ -202,7 +202,7 @@ class Discriminator:
 
       # Note that this is explicitly None because
       # tf.nn.sigmoid.cross_entropy_with_logits is used in the train step.
-      layers.append(tf.reshape(layers[-1], [-1, 4*4*256]))
+      layers.append(tf.reshape(layers[-1], [-1, 7*7*256]))
       layers.append(tf.layers.dense(layers[-1], 1, activation=tf.nn.elu))
       layers.append(tf.layers.batch_normalization(layers[-1], training=training))
       for l in layers: