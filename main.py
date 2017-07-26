708df10d8aa428c19bcea7eae4613e353e931240
Samuel Chin <samuelchin91@gmail.com>
2017-07-26 22:52:38 +0800

    completed argparser

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 import dcgan
 
-def main():
+def main(params):
   config = tf.estimator.RunConfig()
   config._model_dir = '/tmp/dcgan_model'
   config._save_summary_steps = 10
@@ -18,15 +18,8 @@ def main():
                     'global_step': 'global_step'}
   logging_hook = tf.train.LoggingTensorHook(
       tensors=tensors_to_log, every_n_iter=1)
-  params = {
-      'batch_size' : 128,
-      'z_dim' : 100,
-      'learning_rate' : 0.0002,
-      'num_g_steps' : 1,
-      'num_d_steps' : 2,
-      'num_t_steps' : 3}
-  mode = 'mnist'
-  if mode == 'mnist':
+  params['num_t_steps'] = params['num_g_steps'] + params['num_d_steps']
+  if params['data_set'] == 'mnist':
     import mnist.discriminator
     import mnist.generator
     import mnist.input
@@ -39,14 +32,41 @@ def main():
       model_fn=dcgan.dcgan_fn,
       config=config,
       params=params)
-  # dcgan_est.train(input_fn=lambda: params['input_fn'](params),
-  #     hooks=[logging_hook])
+  if params['mode'] == 'train':
+    dcgan_est.train(input_fn=lambda: params['input_fn'](params),
+      hooks=[logging_hook])
+  elif params['mode'] == 'predict':
+    pred = dcgan_est.predict(input_fn=lambda: params['input_fn'](params))
+    print np.array(list(itertools.islice(pred, 1)))[0]['sampled_images'].shape
+    print 'Done'
+  else:
+    raise Exception('Invalid options to mode')
 
-  pred = dcgan_est.predict(input_fn=lambda: params['input_fn'](params))
-  print np.array(list(itertools.islice(pred, 1)))[0]['sampled_images'].shape
-  print 'Done'
-
-def parse_arguments(argv)
+def parse_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-m', '--mode', type=str, default=None,
+    help="mode: train or predict")
+  parser.add_argument('-d', '--data_set', type=str, default=None,
+    help="mnist or etc.")
+  parser.add_argument('-b', '--batch_size', type=int, default=128,
+    help='batch size')
+  parser.add_argument('-z', '--z_dim', type=int, default=100,
+    help='dimension for z vector')
+  parser.add_argument('-l', '--learning_rate', type=float, default=0.0002,
+    help='learning rate for optimizer')
+  parser.add_argument('-i', '--improvements', type=str, default='GAN',
+    help='tweaks and tricks to improve GAN training')
+  parser.add_argument('-ng', '--num_g_steps', type=int, default=1,
+    help='number of g steps to take')
+  parser.add_argument('-nd', '--num_d_steps', type=int, default=1,
+    help='number of d steps to take')
+  parser.add_argument('-cvmax', '--clip_value_min', type=int, default=-0.5,
+    help='weights clip value min for WGAN')
+  parser.add_argument('-cvmin', '--clip_value_max', type=int, default=0.5,
+    help='weights clip value max for WGAN')
+  return parser.parse_args()
 
 if __name__ == '__main__':
-  main()
+  params = vars(parse_arguments())
+  print params
+  main(params)
